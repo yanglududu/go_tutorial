bd3bdb9b1f3d6c0e3ed84a5dd9c0426242fc0e65
yanglududu <yanglududu@163.com>
2018-01-28 00:25:15 +0800

    great

---
+++ struct/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName string
+	contactInfo
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	pointerToPerson.firstName = newFirstName
+}
+
+func (p person) print(){
+	fmt.Println(p.firstName)
+}
+
+func main() {
+	//var alex person
+	//alex.firstName = "alex"
+	//alex.lastName = "armor"
+	//fmt.Printf("%+v", alex)
+	//fmt.Println(alex)
+
+	jim := person{
+		firstName: "alex",
+		lastName: "dudu",
+		contactInfo: contactInfo{
+			email: "duduyang@dy.com",
+			zipCode: 100010,
+		},
+	}
+
+	fmt.Printf("%+v", jim)
+	jimPointer := &jim
+	jimPointer.updateName("jim")
+	jim.print()
+}
